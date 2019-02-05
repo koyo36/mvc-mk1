890a76a2e7b570fd546b99b92aa73cce09e50613
Aleksandar Kojovic <koyocreative@outlook.com>
2019-02-05 09:55:27 +0100

    Added DB connection and Config parser

--- app/init.php
+++ app/init.php
@@ -1,7 +1,22 @@
 <?php
+// CONFIGURATION
+// TODO :: Find better way for this
+$GLOBALS['config'] = array(
+    'mysql'        => array(
+        'host'      => 'localhost',
+        'username'  => 'root',
+        'password'  => '',
+        'dbname'    => 'mvcdb'
+    )
+);
 
 
 require_once 'core/App.php';
 require_once 'core/Controller.php';
 
+require_once 'core/Config.php';
+require_once 'core/DB.php';
+
 $app = new App;
+$DB = DB::getInstance();
+
